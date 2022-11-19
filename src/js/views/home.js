4e0be6967fbbbd0b1f6b85f5b7b3c0ed42715311
chrisperez22 <chrisperez0531@gmail.com>
2022-11-19 00:26:14 +0000

    namemiddle

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,12 +1,13 @@
 import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import googleimage from "../../img/google.jpg";
 
 export const Home = () => {
 	return (
 
 		<div>
-
+		<img class="cover" src={googleimage}/>
 			<div class="wrap">
 				<div class="search">
 					<input className="generate-search" type="text" class="searchTerm" placeholder="What are you looking for?" />
